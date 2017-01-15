6ac11bfdeb68f38dad2dfd9a78ea522c57b7c855
Mateusz Olczyk <matik60@gmail.com>
2017-01-15 21:39:36 +0100

    Initial commit 2

---
+++ src/edu/agh/matik60/regression/DataSet.java
@@ -0,0 +1,15 @@
+package edu.agh.matik60.regression;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by matik60 on 15.01.2017.
+ */
+public class DataSet {
+    int rows;
+    List<String> columns_labels;
+    List<List<Double>> columns_data;
+    List<Double> column_values;
+
+}
---
+++ src/edu/agh/matik60/regression/Main.java
@@ -0,0 +1,8 @@
+package edu.agh.matik60.regression;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
