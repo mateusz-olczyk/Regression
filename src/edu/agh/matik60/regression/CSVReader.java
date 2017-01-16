180b6655b22ce313f9c26d26d3105c695951f9bb
Mateusz Olczyk <matik60@gmail.com>
2017-01-16 09:14:27 +0100

    Added DataSet with method generateFromCSV

--- src/edu/agh/matik60/regression/CSVReader.java
+++ src/edu/agh/matik60/regression/CSVReader.java
@@ -15,6 +15,15 @@ public class CSVReader implements Iterable<Double[]> {
     private String csvFilePath;
     private String delimiter;
     private List<Double[]> data;
+    private int columns;
+
+    public int getColumns() {
+        return columns;
+    }
+
+    public int getRows() {
+        return data.size();
+    }
 
     public String getCsvFilePath() {
         if (csvFilePath == null) return "";
@@ -25,18 +34,22 @@ public class CSVReader implements Iterable<Double[]> {
         csvFilePath = path;
         delimiter = ",";
         data = new ArrayList<>();
+        columns = 0;
+        loadData();
     }
 
-    private void loadData() throws IOException {
+    private void loadData() {
         try (BufferedReader reader = new BufferedReader(new FileReader(getCsvFilePath()))) {
             String line;
             while ((line = reader.readLine()) != null) {
                 String[] strings = line.split(delimiter);
                 int l = strings.length;
+                columns = l;
                 Double[] values = new Double[l];
                 for (int i=0;i<l;i++) {
                     values[i] = Double.parseDouble(strings[i]);
                 }
+                data.add(values);
             }
         } catch (IOException | IllegalArgumentException e) {
             e.printStackTrace();
--- src/edu/agh/matik60/regression/DataSet.java
+++ src/edu/agh/matik60/regression/DataSet.java
@@ -9,10 +9,36 @@ import java.util.List;
 public class DataSet {
 
     private int rows;
-    private int columns_separation; // Determines how many columns from the beginning are treated as external data, the rest are auxiliary
-    private boolean flag_column_one; // Virtual column with ones
-    private List<String> columns_labels;
-    private List<List<Double>> columns_data;
-    private List<Double> column_values;
+    private int columnsSeparation; // Determines how many columns from the beginning are treated as external data, the rest are auxiliary
+    private boolean hasColumnWithOnes; // Virtual column with ones
+    private String[] header; // Columns labels
+    private List<List<Double>> listOfDataRecords;
+    private List<Double> listOfValues;
+
+    public DataSet() {
+        rows = 0;
+        columnsSeparation = 0;
+        hasColumnWithOnes = true;
+        listOfDataRecords = new ArrayList<>();
+        listOfValues = new ArrayList<>();
+    }
+
+    public void generateFromCSV(String path, int columnValuesIndex) {
+        CSVReader r = new CSVReader(path);
+        columnsSeparation = r.getColumns()-1;
+        rows = r.getRows();
+        for (Double[] rowFromCSV : r) {
+            // Prepare next record
+            List<Double> rowData = new ArrayList<>();
+            for (int i=0;i<rowFromCSV.length;i++) {
+                if (i == columnValuesIndex)
+                    listOfValues.add(rowFromCSV[i]);
+                else
+                    rowData.add(rowFromCSV[i]);
+            }
+            // Add record to listOfDataRecords
+            listOfDataRecords.add(rowData);
+        }
+    }
 
 }
