000e0bfc9fe991d1c8cad0ca49f1ba424bce5bd2
Mateusz Olczyk <matik60@gmail.com>
2017-01-18 16:45:17 +0100

    First version of DataChart window

--- src/edu/agh/matik60/regression/Main.java
+++ src/edu/agh/matik60/regression/Main.java
@@ -1,8 +1,30 @@
 package edu.agh.matik60.regression;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Regression");
+
+        try {
+            FXMLLoader loader = new FXMLLoader(Main.class.getResource("gui/DataChart.fxml"));
+            Scene scene = new Scene((AnchorPane) loader.load());
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
