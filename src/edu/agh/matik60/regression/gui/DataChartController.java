515793d3bdf5b8aca6ade59fd1ef81bb97d2b60a
Mateusz Olczyk <matik60@gmail.com>
2017-01-21 00:46:40 +0100

    Showing data on ScatterChart

--- src/edu/agh/matik60/regression/gui/DataChartController.java
+++ src/edu/agh/matik60/regression/gui/DataChartController.java
@@ -5,11 +5,13 @@ import javafx.beans.property.*;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.chart.Axis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.ScatterChart;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Label;
-import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.util.Callback;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,7 +24,9 @@ public class DataChartController {
     @FXML
     private Label labelCSVFilePath;
     @FXML
-    private TableView<List<DoubleProperty>> tableview;
+    private TableView<List<DoubleProperty>> tableView;
+    @FXML
+    private ScatterChart<Number, Number> scatterChart;
 
 //    private ObservableList<IntegerProperty> list = FXCollections.observableArrayList();
 
@@ -31,12 +35,13 @@ public class DataChartController {
     @FXML
     private void handleButtonLoadFromCSV() {
         initTableView();
+        initScatterChart();
     }
 
     @FXML
     private void handleButtonReset() {
         //labelCSVFilePath.setText("Hello");
-//        tableview.getColumns().add(new TableColumn<IntegerProperty,Integer>("first"));
+//        tableView.getColumns().add(new TableColumn<IntegerProperty,Integer>("first"));
 //        list.clear();
     }
 
@@ -45,22 +50,34 @@ public class DataChartController {
         //tablecolumnX.setCellValueFactory(cell -> cell.getValue().asObject());
     }
 
+    private void initScatterChart() {
+        scatterChart.getData().clear();
+        XYChart.Series<Number, Number> series = new XYChart.Series<>();
+        series.setName("Data from CSV");
+        for (int i = 0; i < ds.getRows(); i++) {
+            Double x = ds.getDataRecord(i).get(0);
+            Double y = ds.getValue(i);
+            series.getData().add(new XYChart.Data<>(x, y));
+        }
+        scatterChart.getData().add(series);
+    }
+
     private void initTableView() {
-        tableview.getColumns().clear();
+        tableView.getColumns().clear();
         ds.generateFromCSV("example.csv",1);
 
-        TableColumn<List<DoubleProperty>,Double> tablecolumnValue = new TableColumn<>("value");
-        tableview.getColumns().add(tablecolumnValue);
-        tablecolumnValue.setCellValueFactory(c->c.getValue().get(0).asObject());
+        TableColumn<List<DoubleProperty>,Double> tableColumnValue = new TableColumn<>("value");
+        tableView.getColumns().add(tableColumnValue);
+        tableColumnValue.setCellValueFactory(c->c.getValue().get(0).asObject());
         for (int i = 0; i < ds.getColumns(); i++) {
             TableColumn<List<DoubleProperty>,Double> tablecolumnData = new TableColumn<>(ds.getHeader(i));
-            tableview.getColumns().add(tablecolumnData);
+            tableView.getColumns().add(tablecolumnData);
             final int j = i+1;
             tablecolumnData.setCellValueFactory(c->c.getValue().get(j).asObject());
         }
 
         ObservableList<List<DoubleProperty>> items = FXCollections.observableArrayList();
-        tableview.setItems(items);
+        tableView.setItems(items);
         for (int i = 0; i < ds.getRows(); i++) {
             List<DoubleProperty> item = new ArrayList<>();
             item.add(new SimpleDoubleProperty(ds.getValue(i)));
